e80c6c326d2f01da7be9024c787d603ebabbac01
Sebastián M <smunozmo@gmail.com>
2021-08-02 16:03:42 -0500

    Add Bootstrap, Jest and Font Awesome.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 import './styles.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+export default function sum(a, b) {
+  return a + b;
+}
