f356658c77d58c84e7d7bbc2448cd0055ba2e5df
Sebastián M <smunozmo@gmail.com>
2021-08-03 13:12:21 -0500

    Create hompage list from API.

---
+++ src/homepage.js
@@ -0,0 +1,35 @@
+import createList from './requests.js';
+
+const listContainer = document.getElementById('homepageList');
+
+const populateList = async () => {
+  const itemList = await createList();
+  for (let i = 0; i < itemList.length; i += 1) {
+    const listDiv = document.createElement('div');
+    listDiv.classList.add('card', 'col-sm-3');
+    listContainer.appendChild(listDiv);
+    const listImg = document.createElement('img');
+    listImg.setAttribute('src', itemList[i].strMealThumb);
+    listImg.classList.add('card-img-top', 'pt-2');
+    listDiv.appendChild(listImg);
+    const listInnerDiv = document.createElement('div');
+    listInnerDiv.classList.add('card-body');
+    listDiv.appendChild(listInnerDiv);
+    const listH5 = document.createElement('h5');
+    listH5.classList.add('card-title');
+    listH5.innerText = itemList[i].strMeal;
+    listInnerDiv.appendChild(listH5);
+    const listP = document.createElement('p');
+    listP.innerText = '3 likes';
+    listP.classList.add('card-text');
+    listInnerDiv.appendChild(listP);
+    const listBtn = document.createElement('a');
+    listBtn.classList.add('btn', 'btn-secondary', 'commentBtn');
+    listBtn.setAttribute('id', itemList[i].idMeal);
+    listBtn.setAttribute('href', '#');
+    listBtn.innerText = 'Comment';
+    listInnerDiv.appendChild(listBtn);
+  }
+};
+
+export default populateList;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
 import './styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import populateList from './homepage.js';
+
+populateList();
