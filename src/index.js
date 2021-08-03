d638b4d2e1e47c55e1e96b85153996bfdb74ba56
jmontanholi <jonunes9326@hotmail.com.br>
2021-08-03 14:44:58 -0300

    Finish creating the modal in the HTML and it's functionalities

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './styles.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import createModal from './modalCreation.js';
+
+const commentBtns = document.getElementsByClassName('commentBtn');
+for (let i = 0; i < commentBtns.length; i += 1) {
+  commentBtns[i].addEventListener('click', () => {
+    createModal(commentBtns[i].id);
+  });
+}
