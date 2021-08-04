bd8c7d9d89788c7419283a67310bd7c66cf627cd
Sebastián M <smunozmo@gmail.com>
2021-08-04 17:28:33 -0500

    Create test for items count.

--- src/itemscount.js
+++ src/itemscount.js
@@ -1,7 +1,12 @@
 const displayCount = async (count) => {
-    const navLink = document.getElementById('navLink');
-    navLink.innerText = `All recipes (${count})`;
-    return count;
-  };
+  const navLink = document.getElementById('navLink');
 
-  export default displayCount;
+  if (count === null || count === undefined) {
+    navLink.innerText = 'No recipes available)';
+    return 0;
+  }
+  navLink.innerText = `All recipes (${count})`;
+  return count;
+};
+
+export default displayCount;
--- src/tests/itemsCount.test.js
+++ src/tests/itemsCount.test.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment jsdom
+ */
 import displayCount from '../itemscount.js';
 
 const unmockedFetch = global.fetch;
@@ -18,7 +21,87 @@ afterAll(() => {
 });
 
 test('Counts all items', async () => {
-  const itemsCountTest = await displayCount();
+  document.body.innerHTML = '<div id="navLink"></div>';
+  const object = [{
+    item_id: 53049,
+    likes: 4,
+  }];
+  const objectLength = object.length;
+  const itemsCountTest = await displayCount(objectLength);
   expect(itemsCountTest).toEqual(1);
 });
 
+test('Counts more than 1 item', async () => {
+  document.body.innerHTML = '<div id="navLink"></div>';
+  const object = [
+    {
+      strMeal: 'Apam balik',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg',
+      idMeal: '53049',
+    },
+    {
+      strMeal: 'Apple & Blackberry Crumble',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/xvsurr1511719182.jpg',
+      idMeal: '52893',
+    },
+    {
+      strMeal: 'Apple Frangipan Tart',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/wxywrq1468235067.jpg',
+      idMeal: '52768',
+    },
+    {
+      strMeal: 'Bakewell tart',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/wyrqqq1468233628.jpg',
+      idMeal: '52767',
+    },
+    {
+      strMeal: 'Banana Pancakes',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/sywswr1511383814.jpg',
+      idMeal: '52855',
+    },
+    {
+      strMeal: 'Battenberg Cake',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/ywwrsp1511720277.jpg',
+      idMeal: '52894',
+    },
+    {
+      strMeal: 'BeaverTails',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/ryppsv1511815505.jpg',
+      idMeal: '52928',
+    },
+    {
+      strMeal: 'Blackberry Fool',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/rpvptu1511641092.jpg',
+      idMeal: '52891',
+    },
+    {
+      strMeal: 'Bread and Butter Pudding',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/xqwwpy1483908697.jpg',
+      idMeal: '52792',
+    },
+    {
+      strMeal: 'Budino Di Ricotta',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1549542877.jpg',
+      idMeal: '52961',
+    },
+    {
+      strMeal: 'Canadian Butter Tarts',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/wpputp1511812960.jpg',
+      idMeal: '52923',
+    },
+    {
+      strMeal: 'Carrot Cake',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/vrspxv1511722107.jpg',
+      idMeal: '52897',
+    }];
+  const objectLength = object.length;
+  const itemsCountTest = await displayCount(objectLength);
+  expect(itemsCountTest).toEqual(12);
+});
+
+test('Counts all items', async () => {
+  document.body.innerHTML = '<div id="navLink"></div>';
+  const object = null;
+  const itemsCountTest = await displayCount(object);
+  expect(itemsCountTest).toEqual(0);
+});
