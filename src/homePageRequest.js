a49e7b85475fb761297f9c0148c98c7f7b89b234
jmontanholi <jonunes9326@hotmail.com.br>
2021-08-06 13:57:33 -0300

    Finish new categories implement

--- src/homePageRequest.js
+++ src/homePageRequest.js
@@ -1,15 +1,21 @@
-const getCategory = async () => {
-  const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert';
+const getCategory = async (category) => {
+  const url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`;
   const response = await fetch(url);
   const parsed = await response.json();
   return parsed.meals;
 };
 
-const createList = async () => {
+const createList = async (category) => {
   const list = [];
-  const result = await getCategory();
-  for (let i = 0; i < 12; i += 1) {
-    list.push(result[i]);
+  const result = await getCategory(category);
+  if (result.length > 12) {
+    for (let i = 0; i < 12; i += 1) {
+      list.push(result[i]);
+    }
+  } else {
+    for (let i = 0; i < result.length; i += 1) {
+      list.push(result[i]);
+    }
   }
   return list;
 };
